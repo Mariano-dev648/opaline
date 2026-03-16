226b49ee31cbeafb1ff6038176ec447d7dc4064c
Mariano Da Silva Lemos <lemosmariano336@gmail.com>
2026-03-15 21:33:09 -0300

    feat: catalogo com filtros, busca e pagina de detalhe do produto

--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produto, Categoria
+
 
 def home(request):
     destaques = Produto.objects.filter(ativo=True, destaque=True)[:8]
@@ -11,15 +12,35 @@ def home(request):
         'categorias': categorias,
     })
 
+
 def catalogo(request):
     produtos = Produto.objects.filter(ativo=True)
     categorias = Categoria.objects.filter(ativo=True)
+
+    # Filtro por categoria
+    categoria_slug = request.GET.get('categoria')
+    if categoria_slug:
+        produtos = produtos.filter(categoria__slug=categoria_slug)
+
+    # Busca por nome
+    busca = request.GET.get('q')
+    if busca:
+        produtos = produtos.filter(nome__icontains=busca)
+
     return render(request, 'catalogo.html', {
         'produtos': produtos,
         'categorias': categorias,
+        'busca': busca,
     })
 
+
 def detalhe_produto(request, slug):
-    from django.shortcuts import get_object_or_404
     produto = get_object_or_404(Produto, slug=slug, ativo=True)
-    return render(request, 'detalhe_produto.html', {'produto': produto})
+    relacionados = Produto.objects.filter(
+        categoria=produto.categoria,
+        ativo=True
+    ).exclude(id=produto.id)[:4]
+    return render(request, 'detalhe_produto.html', {
+        'produto': produto,
+        'relacionados': relacionados,
+    })
